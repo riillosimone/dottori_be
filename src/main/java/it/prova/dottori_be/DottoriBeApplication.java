db774825723ea782e07181527dfb1e08102726d1
simoneriillo <riillosimone@gmail.com>
2023-05-26 15:24:57 +0200

    primo commit

---
+++ src/main/java/it/prova/dottori_be/DottoriBeApplication.java
@@ -0,0 +1,13 @@
+package it.prova.dottori_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DottoriBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DottoriBeApplication.class, args);
+	}
+
+}
